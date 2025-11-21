f64a03c77d3e1de8564962c23f232835e29e8863
celezte <theodorejong026@student.unsrat.ac.id>
2025-11-21 19:36:45 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+kontak = {}
+
+def tampilkan_kontak():
+    if not kontak:
+        print("\nBelum ada kontak yang tersimpan.")
+    else:
+        print("\nDaftar Kontak:")
+        for nama, nomor in kontak.items():
+            print(f"- {nama} : {nomor}")
